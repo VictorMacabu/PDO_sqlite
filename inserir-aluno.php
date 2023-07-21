ab22a0cbce35e23356d89c5f88b16ca9981c25da
VictorMacabu <victor.macabu12@gmail.com>
2023-07-21 00:08:54 -0300

    previnindo SQL injection, prepared Statements e tipos de dados

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,8 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Robertinho', new \DateTimeImmutable('1997-12-22'));
+$student = new Student(null,
+    "Maria Clara",
+    new \DateTimeImmutable('2016-07-19'));
 
-$sqlInsert = "INSERT INTO students(name, birth_date) VALUES ('{$student->name()}','{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students(name, birth_date) VALUES (:name,:birth_date);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date',$student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()){
+    echo 'Aluno incluído';
+}
+
+
